3ac25b288a2f0093066f6c9416f2dba2712ff871
Elarif Inzoudine <elarifinzoudine@MacBook-Pro-de-Elarif.local>
2023-11-06 19:49:20 +0100

    First push

---
+++ overlook/src/main/java/com/overlook/overlook/OverlookHotelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.overlook.overlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlookHotelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlookHotelManagementSystemApplication.class, args);
+	}
+
+}
---
+++ overlook/src/test/java/com/overlook/overlook/OverlookHotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overlook.overlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlookHotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
